c5e80a58fd90e2f9117f5cf7afe186eaa88049a0
Ranaivoson James <ranaivosonjames@yahoo.fr>
2018-05-18 11:27:26 +0200

    2 version

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mon SITE</title>
+        <meta charset "utf-8">
+         <link rel="stylesheet" href="style.css">
+         <style>
+                body {background : grey;}
+                p {
+                    position : absolute;
+                    top : 40%;
+                    left : 40%;
+                    font-family : Calibri;
+                    font-size : 40px;
+                }
+                i {
+                    position : absolute;
+                    top : 52%;
+                    left : 32%;
+                    font-family : Calibri;
+                    font-size : 40px;
+                    color : black;
+                }
+         
+         </style>
+    </head>
+    <body>
+        <p>Hello !!!</p>
+        <i>On est vendredi et c'est bientot le week end</i>
+    </body>
+</html>
